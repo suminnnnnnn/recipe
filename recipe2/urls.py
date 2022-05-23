91f2248204b7ef4fda03e2557e47dfc71618f3b2
hkm104 <hkm104@naver.com>
2022-05-23 10:39:31 +0900

    20220523

--- recipe2/urls.py
+++ recipe2/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name='recipe2'
 urlpatterns = [
     path('search/', views.SearchFormView.as_view(), name = 'search'),
 
