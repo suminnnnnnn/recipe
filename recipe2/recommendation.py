1a779e3662e9d7466dc12277efad8986ef710c41
yumyumyumy <104751820+yumyumyumy@users.noreply.github.com>
2022-05-25 12:21:08 +0900

    recipe2 search 추천기능완료

--- recipe2/recommendation.py
+++ recipe2/recommendation.py
@@ -2,25 +2,6 @@ from random import shuffle
 from recipe2.models import recipe
 from stockapp.models import Stock
 
-#사용자 재고 기반 추천
-#사용자 재고 리스트 가져오기
-
-# def get_stock_list():
-#      all_stock = Stock.objects.filter(stock_user=request.user)
-#      stock_list = []
-#      for stock in all_stock:
-#          stock_list.append(stock.stock_stock)
-#     return stock_list 
-
-
-# def get_recipe_list():
-#     all_recipe = recipe.objebts.all()
-#     recommend_recipe_list = []
-#     for recipe in all_recipe:
-#         recommend_recipe_list.append(recipe.recipe)
-#     return recommend_recipe_list
-
-
 #랜덤추천
 def recommend_random():
     my_qset = recipe.objects.all()
