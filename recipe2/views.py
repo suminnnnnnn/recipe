51e66b8cc7a64f843e187c374eff9f9950d41e94
yumyumyumy <104751820+yumyumyumy@users.noreply.github.com>
2022-05-25 15:20:39 +0900

    recipe2 recommend 수정

--- recipe2/views.py
+++ recipe2/views.py
@@ -1,6 +1,9 @@
+from random import random
 from django.shortcuts import render,redirect
 from django.views.generic.edit import FormView
+from requests import request
 from rest_framework.response import Response
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.views import APIView
 from .serializers import RecipeSerializer
 from recipe2.forms import PostSearchForm
@@ -9,35 +12,6 @@ from .models import recipe
 from stockapp.models import Stock
 from recipe2.recommendation import recommend_random
 from django.forms import model_to_dict
-from random import shuffle
-
-#재료기반레시피추천
-class RecipeStockAPIView(APIView):
-    serializer_class = RecipeSerializer
-
-    def get(self, request):
-        all_stock = Stock.objects.filter(stock_user=request.user)
-        query = '''select * from recipe '''
-        sep = ''
-        prefix = 'where '
-        
-        for stock in all_stock:
-            재고 = stock.stock_stock
-            query += prefix + sep + "ingredient like '%%" + 재고 + "%%'"
-            sep = ' and '
-            prefix = ''
-
-        result = recipe.objects.raw(query)
-        # print(result)
-        data_list = []
-        for r in result:
-            data = model_to_dict(r)
-            data_list.append(data)
-        
-        return Response(data_list)
-
-
-
 
 #레시피 검색
 class SearchFormView(FormView):
@@ -57,23 +31,76 @@ class SearchFormView(FormView):
         context['object_list'] = post_list
         return render(self.request, self.template_name, context)
 
+
+#재료기반레시피추천
+def recipeStock(request):
+    all_stock = Stock.objects.filter(stock_user=request.user)
+    query = '''select * from recipe '''
+    sep = ''
+    prefix = 'where '
+    
+    for stock in all_stock:
+        재고 = stock.stock_stock
+        query += prefix + sep + "ingredient like '%%" + 재고 + "%%'"
+        sep = ' and '
+        prefix = ''
+
+    result = recipe.objects.raw(query)
+    # print(result)
+    data_list = []
+    for r in result:
+        data = model_to_dict(r)
+        data_list.append(data)
+
+    result_dict = recommend_random()
+    return render(
+        request, 'recipe2/recommend.html', 
+        {
+            'data_list': data_list,
+            'result_dict': result_dict
+        }
+    )
+
+class RecipeStockAPIView(APIView):
+    serializer_class = RecipeSerializer
+    template_name = 'recommend.html'
+
+    def get(self, request):
+        all_stock = Stock.objects.filter(stock_user=request.user)
+        query = '''select * from recipe '''
+        sep = ''
+        prefix = 'where '
+        
+        for stock in all_stock:
+            재고 = stock.stock_stock
+            query += prefix + sep + "ingredient like '%%" + 재고 + "%%'"
+            sep = ' and '
+            prefix = ''
+
+        result = recipe.objects.raw(query)
+        # print(result)
+        data_list = []
+        for r in result:
+            data = model_to_dict(r)
+            data_list.append(data)
+
+        result_dict = recommend_random()
+
+
+        return Response(data_list, self.template_name)
+        return render
+
+
 #랜덤레시피추천
 
 class RandomAPIView(APIView):
     serializer_class = RecipeSerializer
-    def recommend_random():
-        my_qset = recipe.objects.all()
-        my_list = list(my_qset)
-        shuffle(my_list)
-        recommend_recipe_id_list = []
-        for item in my_list[:20]:
-            print(item.recipe)
-            recommend_recipe_id_list.append(item.recipe)
-        result = dict()
-        result['ids'] = recommend_recipe_id_list
-        return result
+    template_name = 'recommend.html'
 
     def get(self, request, *args, **kwargs):
         result_dict = recommend_random()
         print(result_dict)
-        return Response(result_dict)
+
+  #      return Response(result_dict)
+        return Response({'randomrecipe': result_dict})
+
